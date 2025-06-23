949a6ece54264fcf8eece38f8e969e01991684cd
Jessi Rick <rick08005@gmail.com>
2025-06-23 13:23:30 -0700

    Create run_angsd_abbababa.sh

---
+++ scripts/run_angsd_abbababa.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+#SBATCH --account=latesgenomics
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=angsd
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=0
+
+date
+
+module load gcc
+module load gsl
+module load samtools
+
+/home/jrick/bin/angsd/angsd -doAbbababa2 1 -bam bamlist.all -sizeFile popfile.all -doCounts 1 -out latesWGS_july2020_060221.angsd -anc /project/latesgenomics/reference_genomes/Lates_calcarifer/lcalcarifer_genome_v3_chromosomal.fa -useLast 1 -minQ 20 -minMapQ 30 -p 32 -blockSize 100000 -maxdepth 1000 
+
+date
